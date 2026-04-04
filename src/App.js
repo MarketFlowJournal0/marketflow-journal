4c1c2b361a64fde9da17202c9265c09e055f759a
MarketFlowJournal0 <u1726851581@gmail.com>
2026-04-04 20:42:45 +0200

    fix SupportWidget chat position + restore AuthModal on landing

--- src/App.js
+++ src/App.js
@@ -246,6 +246,7 @@ function AppInner() {
           } />
         </Routes>
         <SupportWidget onOpenPage={() => {}} />
+        {authModal && <AuthModal defaultTab={authModal} onClose={closeAuth} onSuccess={handleAuthSuccess} />}
       </>
     );
   }
